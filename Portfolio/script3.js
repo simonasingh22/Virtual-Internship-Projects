d12261a26a3fa563229fd4937ef2c2db9bbacb77
simonasingh22 <148738453+simonasingh22@users.noreply.github.com>
2026-03-27 17:29:58 +0530

    Delete Portfolio directory

--- Portfolio/script3.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Example: Change the title of the "About Me" section on click
-    const aboutTitle = document.querySelector("#about h2");
-
-    aboutTitle.addEventListener("click", function() {
-        aboutTitle.textContent = "All About Simona Singh";
-    });
-});
